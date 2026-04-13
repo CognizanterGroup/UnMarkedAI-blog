0ec3639e52775da37469fb8d6f6dd0be4a7ae185
CognizanterGroup <cognizanterai@gmail.com>
2026-04-13 08:22:11 +0100

    hello

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,36 @@ export default defineNuxtConfig({
     'nuxt-og-image'
   ],
 
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
   vite: {
     base: '/blog/',
     plugins: [microfrontends()]
